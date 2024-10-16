51f6a8fe42f868a743691ce6edd3d3bcb2279e47
nvhoang2803 <nvhg2803@gmail.com>
2024-10-16 15:10:22 +0700

    add cat

--- lua/pets/pets/cat.lua
+++ lua/pets/pets/cat.lua
@@ -1,23 +1,27 @@
 return {
-    next_actions = {
-        idle = { "idle", "sit", "lie", "walk", "run", "walk_left" },
-        lie = { "lie", "sittolie", "idle", "lietosit" },
-        run = { "run", "walk", "idle", "run_left" },
-        run_left = { "run_left", "walk_left", "idle", "run" },
-        sit = { "sit", "sittolie", "lie", "lietosit", "sittostand" },
-        walk = { "walk", "run", "idle", "walk_left" },
-        walk_left = { "walk_left", "run_left", "idle" },
-    },
-    idle_actions = { "idle", "sit", "lie" },
-    first_action = "idle",
-    movements = {
-        right = {
-            normal = { "walk" },
-            fast = { "run" },
-        },
-        left = {
-            normal = { "walk_left" },
-            fast = { "run_left" },
-        },
-    },
-  }
+  next_actions = {
+      idle = { "idle", "sit", "lie", "walk", "run", "walk_left" },
+      lie = { "lie", "sittolie", "idle", "lietosit" },
+      lietosit = { "lietosit", "sit", "idle" },
+      run = { "run", "walk", "idle", "run_left" },
+      run_left = { "run_left", "walk_left", "idle", "run" },
+      sit = { "sit", "sittolie", "idle", "sittostand" },
+      sittolie = { "sittolie", "lie", "idle" },
+      sittostand = { "sittostand", "idle", "sit", "walk" },
+      standtosit = { "standtosit", "sit", "idle" },
+      walk = { "walk", "run", "idle", "walk_left" },
+      walk_left = { "walk_left", "run_left", "idle" },
+  },
+  idle_actions = { "idle", "sit", "lie" },
+  first_action = "idle",
+  movements = {
+      right = {
+          normal = { "walk" },
+          fast = { "run" },
+      },
+      left = {
+          normal = { "walk_left" },
+          fast = { "run_left" },
+      },
+  },
+}
